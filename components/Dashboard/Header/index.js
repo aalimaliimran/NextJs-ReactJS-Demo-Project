c5f7ef937cc7279e12b9253294174757a5aedfdb
Ali imran <ali.imran@virtualforce.io>
2022-12-23 22:28:04 +0500

    update

--- components/Dashboard/Header/index.js
+++ components/Dashboard/Header/index.js
@@ -6,6 +6,11 @@ import Tooltip from '@mui/material/Tooltip';
 import HelpIcon from '@mui/icons-material/Help';
 import { headerStyles } from './styles';
 import {useRouter} from 'next/router';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import { styled } from '@mui/material/styles';
+import MuiAppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
 
 const Header = () => {
 
@@ -18,10 +23,52 @@ const Header = () => {
       setTitle(parsedTitle);
     }, [router]);
 
+    const [open, setOpen] = React.useState(false);
+    const handleDrawerOpen = () => {
+        setOpen(true);
+      };
+      const drawerWidth = 240;
+      const AppBar = styled(MuiAppBar, {
+        shouldForwardProp: (prop) => prop !== 'open',
+      })(({ theme, open }) => ({
+        zIndex: theme.zIndex.drawer + 1,
+        transition: theme.transitions.create(['width', 'margin'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(open && {
+          marginLeft: drawerWidth,
+          width: `calc(100% - ${drawerWidth}px)`,
+          transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+        }),
+      }));
+
     return (
      <header className='dashboard_header'>
         <div className='row'>
             <div className='col-xl-4 flex_col left_left'>
+            <AppBar position="fixed" open={open}>
+                <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{
+                    marginRight: 5,
+                    ...(open && { display: 'none' }),
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                    Mini variant drawer
+                </Typography>
+                </Toolbar>
+            </AppBar>
                 <h1>{title}</h1>
             </div>
             <div className='col-xl-8 flex_col right_col'>
